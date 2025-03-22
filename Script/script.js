b477e0882df916fb69dd081373f7a623a0c4ebec
TonmoySarkerBD <tonmoycsebd+github@gmail.com>
2025-03-22 18:35:45 +0600

    Load Videos Api

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,5 @@
+//  API Fetch Area
+
 function loadCatagory() {
     // Fetch the data
 
@@ -5,6 +7,14 @@ function loadCatagory() {
         .then((res) => res.json())
         .then((data) => displayCategories(data.categories));
 }
+
+function loadVideos(){
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+}
+
+//  Display API Data Area
 
 function displayCategories(categories) {
     const catContainer = document.getElementById("Category-container");
@@ -17,5 +27,30 @@ function displayCategories(categories) {
     }
 }
 
+function displayVideos(videos){
+    vidContainer = document.getElementById("vid-container");
+    videos.forEach(video => {
+        const vidCard =document.createElement("div");
+        vidCard.innerHTML =`
+        <div class="card bg-base-100 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        `;
+        vidContainer.append(vidCard);
+    });
+}
+
 
 loadCatagory();
+loadVideos();
